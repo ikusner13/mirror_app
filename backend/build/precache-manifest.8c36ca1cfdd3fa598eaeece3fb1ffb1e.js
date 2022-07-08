0445d27f38e497e910bf1796553b1e136f02ff95
ian <iankush1317@gmail.com>
2022-07-07 21:10:06 -0400

    new fe build

---
+++ backend/build/precache-manifest.8c36ca1cfdd3fa598eaeece3fb1ffb1e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02f22eee866d30ea86e7d94c21bdab1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "254f1a2af0e6caf4c408",
+    "url": "./static/css/2.626c4881.chunk.css"
+  },
+  {
+    "revision": "0f44bfff2cdeddc7a09f",
+    "url": "./static/css/main.5a609ffb.chunk.css"
+  },
+  {
+    "revision": "254f1a2af0e6caf4c408",
+    "url": "./static/js/2.401ff12d.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "./static/js/2.401ff12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f44bfff2cdeddc7a09f",
+    "url": "./static/js/main.80367a4b.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "./static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "./static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "./static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "./static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "./static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
